006c4ea27b67c94e1b2703825ec939dbc59c3adb
Shubham Lodhi <48179404+shubhamlodhi@users.noreply.github.com>
2023-04-29 21:59:29 +0530

    add : Spring Initializr

---
+++ src/main/java/com/codewithlodhi/yellowcoder/YellowcoderApplication.java
@@ -0,0 +1,14 @@
+package com.codewithlodhi.yellowcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowcoderApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(YellowcoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithlodhi/yellowcoder/YellowcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithlodhi.yellowcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowcoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
